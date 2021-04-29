078d49543263529eda756cb6d92ea2f0cb792be9
Gwon Hyuksang <gwonhyuksang@gmail.com>
2021-04-29 04:32:48 +0000

    re

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-apt-get update  || { echo 'failed' ; exit 1; }
-
-apt-get install \
+sudo apt-get update && apt-get install \
   apt-transport-https \
   ca-certificates \
   curl \
@@ -10,11 +8,11 @@ apt-get install \
   lsb-release \
   || { echo 'apt install failed' ; exit 1; }
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo 'adding key failed' ; exit 1; }
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo 'adding key failed' ; exit 1; }
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
   || { echo 'adding docker to sources list failed' ; exit 1; }
 
 apt-get update && apt-get install docker-ce docker-ce-cli containerd.io || { echo 'installation failed' ; exit 1; }
