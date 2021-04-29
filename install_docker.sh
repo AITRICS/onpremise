5e64a7f320c4956904a6061267aa1d0512604462
Gwon Hyuksang <gwonhyuksang@gmail.com>
2021-04-29 03:44:07 +0000

    re

--- install_docker.sh
+++ install_docker.sh
@@ -8,14 +8,13 @@ apt-get install \
   curl \
   gnupg \
   lsb-release \
-  || { echo 'failed' ; exit 1; }
+  || { echo 'apt install failed' ; exit 1; }
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  || { echo 'my_command failed' ; exit 1; }
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  || { echo 'adding key failed' ; exit 1; }
 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
-  || { echo 'failed' ; exit 1; }
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null \
+  || { echo 'adding docker to sources list failed' ; exit 1; }
 
-apt-get update || { echo 'failed' ; exit 1; }
-apt-get install docker-ce docker-ce-cli containerd.io || { echo 'failed' ; exit 1; }
+apt-get update && apt-get install docker-ce docker-ce-cli containerd.io || { echo 'installation failed' ; exit 1; }
