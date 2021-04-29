62a561279b37279a84aacc7f83ebd3b83438bbcd
Gwon Hyuksang <gwonhyuksang@gmail.com>
2021-04-29 03:48:41 +0000

    re

--- install_docker.sh
+++ install_docker.sh
@@ -10,10 +10,10 @@ apt-get install \
   lsb-release \
   || { echo 'apt install failed' ; exit 1; }
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  || { echo 'adding key failed' ; exit 1; }
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo 'adding key failed' ; exit 1; }
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null \
   || { echo 'adding docker to sources list failed' ; exit 1; }
 
